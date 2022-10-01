003713f4c3d4ba372a62c63033aec37ece855df0
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-10-01 08:50:56 -0300

    Inserido a descrição

---
+++ script.js
@@ -0,0 +1,4 @@
+const description = document.getElementById("description");
+const textDescription =  "\"Você se encontra de frente ao tesouro da grande esfinge, para abrir a passagem e obter todo o seu ouro e riqueza tudo o que você precisa fazer é acertar a uma charada.\"";
+
+description.innerText = textDescription;
